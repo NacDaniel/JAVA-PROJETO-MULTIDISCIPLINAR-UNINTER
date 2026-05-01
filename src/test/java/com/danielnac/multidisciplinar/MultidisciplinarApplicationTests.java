e030c4a3e6d548dbc04c134c1d34934e273eb006
danielRnkme <daniel.coutinho@rankme.com.br>
2026-05-01 10:32:14 -0300

    Arquivos iniciais gerados pelo Spring Initializr

---
+++ src/test/java/com/danielnac/multidisciplinar/MultidisciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnac.multidisciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidisciplinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
