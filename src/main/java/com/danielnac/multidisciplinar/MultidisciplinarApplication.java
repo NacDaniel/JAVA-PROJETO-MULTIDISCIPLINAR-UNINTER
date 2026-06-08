9f76c2fb26ac3c0324ec15826c4c2e802dd97b5a
danielRnkme <daniel.coutinho@rankme.com.br>
2026-06-08 14:43:06 -0300

    Arquivos iniciais gerados pelo Spring Initializr

---
+++ src/main/java/com/danielnac/multidisciplinar/MultidisciplinarApplication.java
@@ -0,0 +1,13 @@
+package com.danielnac.multidisciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidisciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidisciplinarApplication.class, args);
+	}
+
+}
